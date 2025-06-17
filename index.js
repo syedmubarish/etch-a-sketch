afe7c9683d7e7b8f9fdb7cec0464ed9bcf352c58
Sayed Mubarish <mubarish007@gmail.com>
2025-06-17 13:19:54 +0530

    Refactor code to get exact same width
    new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+const changeBtn = document.querySelector(".btn-container button")
+
 function createSquareDivs(number) {
     for (let rows = 0; rows < number; rows++) {
         const row = document.createElement("div");
@@ -6,7 +8,11 @@ function createSquareDivs(number) {
         for (let cols = 0; cols < number; cols++) {
             const col = document.createElement("div");
             col.classList.add("col-div")
+            col.style.width = `${700 / number}px`
+            col.style.height = `${700 / number}px`
+            
             //   col.textContent=cols
+            
             row.appendChild(col)
         }
         
@@ -16,15 +22,32 @@ function createSquareDivs(number) {
 
 createSquareDivs(16);
 
-const rows = Array.from(document.querySelectorAll(".row-div"))
 
-for(const row of rows){
-    row.addEventListener("mouseover",(e)=>{
-        e.target.style.backgroundColor = "grey"
-    })
+function doPainting(){
+    const rows = Array.from(document.querySelectorAll(".row-div"))
+    for(const row of rows){
+        row.addEventListener("mouseover",(e)=>{
+            e.target.style.backgroundColor = "grey"
+        })
+    }
 }
 
+doPainting()
 
-
-
-
+changeBtn.addEventListener('click',()=>{
+    
+    const gridSize = prompt("Enter grid size",16)
+    
+    
+    if(gridSize.trim()==""||isNaN(+gridSize)||gridSize>100){
+        alert("Enter valid number less than 100");
+        return
+    }
+    const parsedGridSize = +gridSize
+    const rows = Array.from(document.querySelectorAll(".row-div"))
+    for(const row of rows){
+        row.remove()
+    }
+    createSquareDivs(parsedGridSize)
+    doPainting()
+})
