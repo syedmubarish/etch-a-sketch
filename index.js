4d82b20ee309149d504f812ef90a147d7697404c
Sayed Mubarish <mubarish007@gmail.com>
2025-06-17 11:04:11 +0530

    Add hover effect
    add event listener to paint divs when mouse hover over them

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const container = document.querySelector(".container");
-
 function createSquareDivs(number) {
-  for (let rows = 0; rows < number; rows++) {
-    const row = document.createElement("div");
-    row.classList.add("row-div");
-    for (let cols = 0; cols < number; cols++) {
-      const col = document.createElement("div");
-      col.classList.add("col-div")
-    //   col.textContent=cols
-      row.appendChild(col)
+    for (let rows = 0; rows < number; rows++) {
+        const row = document.createElement("div");
+        row.classList.add("row-div");
+        for (let cols = 0; cols < number; cols++) {
+            const col = document.createElement("div");
+            col.classList.add("col-div")
+            //   col.textContent=cols
+            row.appendChild(col)
+        }
+        
+        container.appendChild(row);
     }
-
-    container.appendChild(row);
-  }
 }
 
 createSquareDivs(16);
+
+const rows = Array.from(document.querySelectorAll(".row-div"))
+
+for(const row of rows){
+    row.addEventListener("mouseover",(e)=>{
+        e.target.style.backgroundColor = "grey"
+    })
+}
+
+
+
+
+
