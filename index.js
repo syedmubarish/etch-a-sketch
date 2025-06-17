812eb11cc65c7539bbf972016a394c1e5011aee6
Sayed Mubarish <mubarish007@gmail.com>
2025-06-17 10:45:07 +0530

    create 16*16 square divs
    add responsiveness

---
+++ index.js
@@ -0,0 +1,18 @@
+const container = document.querySelector(".container");
+
+function createSquareDivs(number) {
+  for (let rows = 0; rows < number; rows++) {
+    const row = document.createElement("div");
+    row.classList.add("row-div");
+    for (let cols = 0; cols < number; cols++) {
+      const col = document.createElement("div");
+      col.classList.add("col-div")
+    //   col.textContent=cols
+      row.appendChild(col)
+    }
+
+    container.appendChild(row);
+  }
+}
+
+createSquareDivs(16);
